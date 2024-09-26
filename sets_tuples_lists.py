ed5e7e5fa3ca97a20ad4fdc6cf3a97e91e30b9b0
Barrios <sabarrios3@cps.k12.il.us>
2024-09-26 08:51:36 -0500

    new changes

--- sets_tuples_lists.py
+++ sets_tuples_lists.py
@@ -20,9 +20,37 @@ for fruit in fruits:
      print (fruit)
     # YOU CAN CHANGE VALUES USING THE FUNCTION ABOVE
      
-fruits.append ("pineapple")
-fruits.remove ("apple")
-fruits.index (0,"pineapple")
-fruits.sort()
-fruits.reverse()
-fruits.clear()
+#fruits.append ("pineapple")
+#fruits.remove ("apple")
+#ruits.index (0,"pineapple")
+#fruits.sort()
+#fruits.reverse()
+#fruits.clear()
+
+#THE TOP SECTION WAS JUST PRACTICE
+cars= ["bwm", "maserati", "audi", "mercedes", "ferrari"]
+print(f"there are a list of {cars}")
+print(f"the first of these cars is {cars[0]}")
+
+cars [0] = "Toyota"
+print (f"the first of these cars is {cars[0]}")
+
+print (f"the last cars is {cars[-1]}")
+cars [-1] = "lamborghini"
+print ("the last of these cars is {cars} ")
+
+cars.append ("Honda")
+print (cars)
+cars.remove ("maserati")
+print (cars)
+
+for car in cars:
+     #print (len(car))
+     #print(car)
+    carRequest = input ("add a new car please: ")
+    cars.append(carRequest)
+    print(cars)
+    if len(cars) > 10:
+         break
+    #YOU HAVE TO TAB IT OVER FOR IT TO BE CONSIDRED A LOOP
+    
